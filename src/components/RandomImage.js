e7997078d598976d001427deab3e92d4f860bf41
Evert Smit <evert@smit.li>
2024-06-26 14:10:27 +0200

    reduced lag on diplaing image

--- src/components/RandomImage.js
+++ src/components/RandomImage.js
@@ -7,7 +7,8 @@ import './RandomImage.css';
 const RandomImage = ({ setImageId, setFetchImage }) => {
   const { getAccessTokenSilently, isAuthenticated, loginWithRedirect } = useAuth0();
   const [imageData, setImageData] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true); // Start with loading true to show spinner initially
+  const [tempImageData, setTempImageData] = useState(null); // Temporary state to hold new image data
   const apiUrl = process.env.REACT_APP_API_URL;
 
   const fetchImage = useCallback(async () => {
@@ -19,14 +20,12 @@ const RandomImage = ({ setImageId, setFetchImage }) => {
           Authorization: `Bearer ${token}`,
         },
       });
-      setImageData(response.data);
-      setImageId(response.data.id); // Pass the image ID to the parent component
+      setTempImageData(response.data); // Set the temporary image data
     } catch (error) {
       console.error('Error fetching the image:', error);
-    } finally {
-      setLoading(false);
+      setLoading(false); // Stop loading on error
     }
-  }, [apiUrl, getAccessTokenSilently, setImageId]);
+  }, [apiUrl, getAccessTokenSilently]);
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -37,23 +36,27 @@ const RandomImage = ({ setImageId, setFetchImage }) => {
     }
   }, [isAuthenticated, loginWithRedirect, fetchImage, setFetchImage]);
 
+  const handleImageLoad = () => {
+    setImageData(tempImageData); // Update the actual image data when the image has loaded
+    setImageId(tempImageData.id); // Pass the image ID to the parent component
+    setLoading(false); // Update state when image is loaded
+  };
+
   return (
     <div className="image-container">
       {loading && (
         <div className="spinner">
-          <ClipLoader color="#000" loading={loading} size={50} />
+          <ClipLoader color="#000" loading={loading} size={150} />
         </div>
       )}
-      {imageData && (
+      {tempImageData && (
         <img
-          src={imageData.image_url}
-          alt={imageData.id}
-          onClick={fetchImage}
-          className={loading ? 'loading' : ''}
+          src={tempImageData.image_url}
+          alt={tempImageData.id}
+          onLoad={handleImageLoad}
+          onClick={!loading ? fetchImage : undefined}
+          style={{ display: loading ? 'none' : 'block', maxWidth: '100%', height: 'auto' }} // Hide image until fully loaded
         />
-      )}
-      {!imageData && !loading && (
-        <p>Loading image...</p>
       )}
     </div>
   );
